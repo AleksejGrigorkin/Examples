765bc65767c6545051270ef1f5dac19f4d12fd32
Grigorkin Aleksej <a1234@74.ru>
2022-08-17 01:16:28 +0500

    Пример 6 Гири

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,12 @@
+﻿int a=1, b=3, c=6, d=8, e=5;
+
+int max=a;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+
+Console.Write("max=");
+Console.WriteLine(max);
