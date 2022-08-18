e71191c7c0ab4b6129264e76e85df8acf79d9826
Grigorkin Aleksej <a1234@74.ru>
2022-08-18 23:16:41 +0500

    Четыре задачи семинара

--- Primer/Program.cs
+++ Primer/Program.cs
@@ -1,7 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.Write("Введите число 1: ");
+int number1=Convert.ToInt32 (Console.ReadLine ()); // Конвертация в число
+Console.Write("Введите число 2: ");
+int number2=Convert.ToInt32 (Console.ReadLine ()); // Конвертация в число
+int result=number2*number2;
+if(result ==number1 )
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}    
+else 
+{
+    Console.WriteLine("Второе число НЕ является квадратом первого");
+}
 
 
-int number=Convert.ToInt32 (Console.ReadLine ()); // Конвертация в число
-int result=number*number;
-Console.WriteLine(result );
+Console.Write("Введите номер дня недели: ");
+int day=Convert.ToInt32 (Console.ReadLine ()); // Конвертация в число
 
+if(day ==1 )
+{
+    Console.WriteLine("Понедельник");
+}    
+else if(day ==2 )
+{
+    Console.WriteLine("Вторник");
+}
+else if(day ==3 )
+{
+    Console.WriteLine("Среда");
+}
+else if(day ==4 )
+{
+    Console.WriteLine("Четверг");
+}
+else if(day ==5 )
+{
+    Console.WriteLine("Пятница");
+}
+else if(day ==6 )
+{
+    Console.WriteLine("Суббота");
+}
+else if(day ==7 )
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Вы ошиблись");
+}
+
+
+
+
+Console.Write("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int N1 = -N;
+
+while (N1 <= N)
+{
+    Console.WriteLine(N1);
+        N1 = N1 + 1;
+}
+
+
+Console.Write("Введите трёхзначное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int last_number = m%10;
+
+Console.WriteLine(last_number);
