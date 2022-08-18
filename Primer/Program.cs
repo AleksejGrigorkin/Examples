50496aae6ae01239eb20c9d2873a84acc3c6a4dd
Grigorkin Aleksej <a1234@74.ru>
2022-08-18 22:26:26 +0500

    q

---
+++ Primer/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int number=Convert.ToInt32 (Console.ReadLine ()); // Конвертация в число
+int result=number*number;
+Console.WriteLine(result );
+
