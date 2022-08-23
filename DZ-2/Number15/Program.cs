c137fcb40e37fc9525c170f2d58a79332dfbff49
Grigorkin Aleksej <a1234@74.ru>
2022-08-23 20:25:18 +0500

    ДЗ-2

---
+++ DZ-2/Number15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+home:
+Console.Write("Введите номер дня недели: ");
+int day=Convert.ToInt32 (Console.ReadLine ());
+if(day ==6^day==7 )
+{
+    Console.WriteLine("Выходной");
+}    
+else if(day <1^day>7 )
+{
+    Console.WriteLine("Вы ошиблись");
+    goto home;
+}
+else
+{
+    Console.WriteLine("Рабочий");
+}
