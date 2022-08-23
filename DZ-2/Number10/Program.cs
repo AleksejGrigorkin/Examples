022faf8055162c97bfac60f22cee19124b9dba36
Grigorkin Aleksej <a1234@74.ru>
2022-08-23 20:30:43 +0500

    ДЗ-2 три задачи

---
+++ DZ-2/Number10/Program.cs
@@ -0,0 +1,21 @@
+﻿// С использованием метода
+Console.WriteLine("Программа принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+home:
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // Конвертация в число
+if (number / 100 == 0 ^ number / 100 > 9)
+{
+    Console.WriteLine("Вы ввели НЕ трёхзначное число:");
+    goto home;
+}
+Console.WriteLine($"Вторая цифра {FindNumberTo(number)}");
+
+int FindNumberTo(int number)
+{
+    int secondnumber = number / 10 % 10;
+    return secondnumber;
+}
+
+// Минималистский вариант
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Вторая цифра {number / 10 % 10}");
---
+++ DZ-2/Number13/Program.cs
@@ -0,0 +1,27 @@
+﻿// С использованием метода
+Console.WriteLine("Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.Write("Введите число: ");
+string str = Console.ReadLine();
+if (int.Parse (str)/100 == 0)
+{
+    Console.WriteLine("В этом числе нет третьей цифры.");
+}
+else
+{
+    Console.WriteLine($"Третья цифра {FindNumberThree(str, 3)}");
+}
+
+
+int FindNumberThree(string str, int index)
+{
+    int number = Convert.ToInt32(new string( str[index - 1], 1));
+    return number;
+}
+
+
+
+// Минималистский вариант
+// string str = Console.ReadLine();
+// if (str.Length < 3) Console.WriteLine("В этом числе нет третьей цифры.");
+// else Console.WriteLine($"Третья цифра {Convert.ToInt32(new string( str[2], 1))}");
+
