2a8f8732b9b27df03ea0217aeefde4f704d940c3
Grigorkin Aleksej <a1234@74.ru>
2022-08-23 20:58:34 +0500

    ДЗ-2 все с методом

--- DZ-2/Number15/Program.cs
+++ DZ-2/Number15/Program.cs
@@ -1,12 +1,33 @@
-﻿Console.WriteLine("Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+﻿// Без использования метода - нафиг он тут нужен
+// Console.WriteLine("Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+// home:
+// Console.Write("Введите номер дня недели: ");
+// int day=Convert.ToInt32 (Console.ReadLine ());
+// if(day ==6^day==7 )
+// {
+//     Console.WriteLine("Выходной");
+// }    
+// else if(day <1^day>7 )
+// {
+//     Console.WriteLine("Вы ошиблись");
+//     goto home;
+// }
+// else
+// {
+//     Console.WriteLine("Рабочий");
+// }
+
+
+//С использованием метода - но это всё усложняет
+Console.WriteLine("Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
 home:
 Console.Write("Введите номер дня недели: ");
-int day=Convert.ToInt32 (Console.ReadLine ());
-if(day ==6^day==7 )
+int day = Convert.ToInt32(Console.ReadLine());
+if (FindWeekend(day) == true)
 {
     Console.WriteLine("Выходной");
-}    
-else if(day <1^day>7 )
+}
+else if (FindMistake(day) == true)
 {
     Console.WriteLine("Вы ошиблись");
     goto home;
@@ -15,3 +36,20 @@ else
 {
     Console.WriteLine("Рабочий");
 }
+
+bool FindWeekend(int day)
+{
+    bool Day = day == 6 ^ day == 7;
+    return Day;
+}
+
+bool FindMistake(int day)
+{
+    bool Day = day < 1 ^ day > 7;
+    return Day;
+}
+
+//минималистский
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day == 6 ^ day == 7) Console.WriteLine("Выходной");
+
