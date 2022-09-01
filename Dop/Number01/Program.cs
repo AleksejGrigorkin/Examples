780cb7074a9b7e01fefa687dd55ae79ccab6e91d
Grigorkin Aleksej <a1234@74.ru>
2022-09-01 12:24:36 +0500

    Задачи от Алёны

---
+++ Dop/Number01/Program.cs
@@ -0,0 +1,25 @@
+﻿//Программа принимает на вход строку, состоящую только из цифр. Выдаёт количество нулей ("0") в начале строки.
+using static System.Console;
+string StrNum = InputString("Введите строку из цифр: ");
+string StrNumVar=StrNum;
+int length=StrNumVar.Length;
+int sum=0;
+while(StrNumVar.Substring(0,1)=="0")
+{
+    sum=sum+1;
+    if(sum==length) {break;}
+    StrNumVar=StrNumVar.Substring(1);
+}
+WriteLine($"Вначале строки нулей: {sum} ");
+
+string InputString(string text)
+{
+    string? x = null;
+    while (x==null||x=="")
+    {
+        Write(text);
+        x = ReadLine();
+        if (x=="") { WriteLine("Значение не должно быть пустым"); }
+    }
+    return x;
+}
---
+++ Dop/Number02/Program.cs
@@ -0,0 +1,28 @@
+﻿//Програсса получает на вход строку символов. Разделяет её на массив строк по два символа.
+//Если строка содержит нечетное количество символов, добавляется "_" - второй символ последней пары.
+//Длина строки от 0 до 100 символов.
+using static System.Console;
+string Str = InputStringMod("Введите строку до 100 символов: ");
+string StrVar=Str;
+int length=StrVar.Length;
+if (length%2!=0) {StrVar=StrVar+"_";}
+length=(length+1)/2;
+string[] s=new string[length];
+for(int i=0;i<length;i++)
+{
+    s[i]=StrVar.Substring(0,2);
+    StrVar=StrVar.Substring(2);
+}
+WriteLine(string.Join(", ", s));
+
+string InputStringMod(string text)
+{
+    string? x = null;
+    while (x==null||x.Length>100)
+    {
+        Write(text);
+        x = ReadLine();
+        if (x.Length>100) { WriteLine("Слишком длинная строка"); }
+    }
+    return x;
+}
