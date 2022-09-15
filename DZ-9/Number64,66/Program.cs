1beb3d0c87c42d787c0c98f67390bb4ac2c769b3
Grigorkin Aleksej <a1234@74.ru>
2022-09-16 00:46:56 +0500

    ДЗ-9

---
+++ DZ-9/Number64,66/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 64.
+// Программа выводит все натуральные числа от M до N.
+// Задача 66.
+// Программа находит сумму натуральных элементов от M до N.
+// Дополнительное условие - всё надо сделать через рекурсию.
+using static System.Console;
+string text1 = "Введите начальное натуральное число: ";
+string text2 = "Натуральное число ВСЕГДА больше нуля";
+int M = InputNaturalNumber(text1, text2);
+text1 = "Введите конечное натуральное число: ";
+int N = InputNaturalNumber(text1, text2);
+WriteLine($"Натуральные числа от {M} до {N}: ");
+Console.WriteLine(NumbersRec(M, N));
+WriteLine();
+// int sum = (M + N) * (N - M + 1) / 2;
+WriteLine($"Сумма натуральных чисел от {M} до {N} = {SumRec(M, N)}");
+WriteLine();
+
+string NumbersRec(int M, int N)
+// выводит числа от M до N, где M<=N - через рекурсию.
+{
+    if (M < N) return $"{M} " + NumbersRec(M + 1, N);
+    else return $"{M}";
+}
+
+int SumRec(int M, int N)
+// Сумма чисел от M до N - через рекурсию.
+{
+    if (N == M) return M;
+    else return N + SumRec(M, N - 1);
+}
+
+
+int InputNaturalNumber(string text1, string text2)
+// Ввод натурального числа.
+{
+    int num = -1;
+    while (num < 1)
+    {
+        num = InputInt(text1);
+        if (num < 1) { WriteLine(text2); }
+    }
+    return num;
+}
+
+int InputInt(string text)
+// Ввод целого числа.
+{
+    bool isNumber = false; int x = 0;
+    while (!isNumber)
+    {
+        Write(text);
+        isNumber = int.TryParse(ReadLine(), out x);
+        if (!isNumber) { WriteLine("Это не число"); }
+    }
+    return x;
+}
---
+++ DZ-9/Number68/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 68.
+// Программа вычисляет функцию Аккермана с помощью рекурсии.
+using static System.Console;
+string text1 = "Введите первый аргумент функции Аккермана: ";
+string text2 = "Аргументы не должны быть меньше нуля";
+int M = InputNaturalNumberPlusNull(text1, text2);
+text1 = "Введите второй аргумент функции Аккермана: ";
+int N = InputNaturalNumberPlusNull(text1, text2);
+int[] A = new int[2];
+A[0] = M;
+A[1] = N;
+Akkerman(A);
+WriteLine($"Akkerman({M},{N}) = {A[1]}");
+
+void Akkerman(int[] A)
+// Функция Аккермана
+{
+    if (A[0] == 0) { A[1]++; }
+    else if (A[1] == 0 && A[0] > 0)
+    {
+        A[0]--;
+        A[1] = 1;
+        Akkerman(A);
+    }
+    else
+    {
+        int mm = A[0] - 1;
+        A[1]--;
+        Akkerman(A);
+        A[0] = mm;
+        Akkerman(A);
+    }
+}
+
+int InputNaturalNumberPlusNull(string text1, string text2)
+// Ввод натурального числа + 0.
+{
+    int num = -1;
+    while (num < 0)
+    {
+        num = InputInt(text1);
+        if (num < 0) { WriteLine(text2); }
+    }
+    return num;
+}
+
+int InputInt(string text)
+// Ввод целого числа.
+{
+    bool isNumber = false; int x = 0;
+    while (!isNumber)
+    {
+        Write(text);
+        isNumber = int.TryParse(ReadLine(), out x);
+        if (!isNumber) { WriteLine("Это не число"); }
+    }
+    return x;
+}
